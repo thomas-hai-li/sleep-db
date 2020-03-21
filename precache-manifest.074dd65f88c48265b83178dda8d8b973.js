fa38aa94d5948a1476b2e0e3aad0b3c72d0b06bc
Thomas H Li <thomas.hai.li98@gmail.com>
2020-03-20 23:32:59 -0400

    Updates

---
+++ precache-manifest.074dd65f88c48265b83178dda8d8b973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f52f38f03b091123741d36db7ff7659",
+    "url": "/react-sleep-tracker/index.html"
+  },
+  {
+    "revision": "034ab14a2f13a5a86eeb",
+    "url": "/react-sleep-tracker/static/css/main.67a9fdd5.chunk.css"
+  },
+  {
+    "revision": "d0f70bc871ce4b4a02f2",
+    "url": "/react-sleep-tracker/static/js/2.53043f1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sleep-tracker/static/js/2.53043f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034ab14a2f13a5a86eeb",
+    "url": "/react-sleep-tracker/static/js/main.aacb25ee.chunk.js"
+  },
+  {
+    "revision": "e083b35e54e3f619fdae",
+    "url": "/react-sleep-tracker/static/js/runtime-main.45463a1b.js"
+  }
+]);
