ba51b609c69f2bfa0e5e7202acb4fb4c49aa3e47
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2021-10-25 21:30:16 -0400

    Updates

---
+++ precache-manifest.c5fb84beda6d16196176c3375b9a8a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9234f693b5937fefffa879272c6fb88",
+    "url": "/sleep-db/index.html"
+  },
+  {
+    "revision": "f22b43115b39274d553b",
+    "url": "/sleep-db/static/css/main.6237e225.chunk.css"
+  },
+  {
+    "revision": "d0f70bc871ce4b4a02f2",
+    "url": "/sleep-db/static/js/2.53043f1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sleep-db/static/js/2.53043f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22b43115b39274d553b",
+    "url": "/sleep-db/static/js/main.790ee97a.chunk.js"
+  },
+  {
+    "revision": "3e8210d02a9c2de0a169",
+    "url": "/sleep-db/static/js/runtime-main.416b42f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b406128da1951a083267c1de1bb0fa17.js"
+  "/sleep-db/precache-manifest.c5fb84beda6d16196176c3375b9a8a1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sleep-db/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
